eaf5962dac0f82a0990dfc4e499a321c6259530a
yunlong.wang <ylwang9627@foxmail.com>
2021-04-18 11:24:52 +0800

    yl #0000 relect+simple facotry, add Properties class use get DB type

--- src/AFdbdemo/reflectaddsimplefactory/DBMain.java
+++ src/AFdbdemo/reflectaddsimplefactory/DBMain.java
@@ -4,10 +4,25 @@ import AFdbdemo.reflectaddsimplefactory.PO.User;
 import AFdbdemo.reflectaddsimplefactory.factory.DataAccess;
 import AFdbdemo.reflectaddsimplefactory.product.department.IDepartment;
 import AFdbdemo.reflectaddsimplefactory.product.user.IUser;
+import com.sun.org.apache.xml.internal.security.Init;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class DBMain {
+    static {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("src/AFdbdemo/reflectaddsimplefactory/setting.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        DataAccess.DB = properties.getProperty("DB");
+    }
+
     public static void main(String[] args) {
-        DataAccess.DB = "Sqlserver";
         IUser user = DataAccess.createUser();
         user.insert(new User(1, "new user" + DataAccess.DB));
 
