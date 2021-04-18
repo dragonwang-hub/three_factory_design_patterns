927befc8386ad3e0cdb824d64f417bd34ae542f6
yunlong.wang <ylwang9627@foxmail.com>
2021-04-18 22:29:52 +0800

    yl #0000 update

--- src/AFshapedemo/abstractfactory/product/circle/GreyCircle.java
+++ src/AFshapedemo/abstractfactory/product/circle/GreyCircle.java
@@ -1,6 +1,6 @@
 package AFshapedemo.abstractfactory.product.circle;
 
-public class GreyCircle extends Circle {
+public class  GreyCircle extends Circle {
     @Override
     public void draw() {
         System.out.println("Draw Grey Circle!");
